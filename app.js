c7cdb57664008848c58f8e2de7c83a8a3065ba0b
Paulo Orquillo <paulo.orquillo@gmail.com>
2018-04-07 17:24:45 +1200

    "Basic crud complete"

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Connect to db
 mongoose.connect('mongodb://localhost/nodekb');
@@ -25,41 +26,22 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended : false}));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set static- public folder
+app.use('/static', express.static('public'));
+
 // Home route
 app.get('/', function(req, res){
-/*  let articles = [
-    {
-      id:1,
-      title:"Article One",
-      author: "Padma Orquillo",
-      body:"This is article one"
-    },
-    {
-      id:2,
-      title:"Article Two",
-      author: "Amber Orquillo",
-      body:"This is article two"
-    },
-    {
-      id:3,
-      title:"Article three",
-      author: "Pauline Orquillo",
-      body:"This is article three"
-    },
-    {
-      id:3,
-      title:"Article four",
-      author: "Penny Orquillo",
-      body:"This is article four"
-    }
-  ];
-*/
   Article.find({}, function(err, articles) {
     if(err) {
       console.log(err);
     } else {
       res.render('index', {
-        title : 'Hello',
+        title : 'Articles',
         articles: articles
       });
     }
@@ -72,6 +54,74 @@ app.get('/articles/add', function(req, res) {
   res.render('add_articles', {
     title : 'Add article'
   });
+});
+
+// get single article
+app.get('/article/:id', function(req, res) {
+  Article.findById(req.params.id, function (err, article) {
+    res.render('article', {
+      article : article
+    });
+  });
+
+});
+
+// load edit form
+app.get('/article/edit/:id', function(req, res) {
+  Article.findById(req.params.id, function (err, article) {
+    res.render('edit-article', {
+      title : "Edit Article",
+      article : article
+    });
+  });
+});
+
+
+// Add Articles POST route
+app.post('/articles/add', function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+// Add Articles POST route
+app.post('/article/edit/:id', function(req, res) {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id};
+
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+app.delete('/article/:id', function(req, res) {
+  let query = {_id:req.params.id}
+
+  Article.remove(query, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    res.send('Success');
+  })
 });
 
 // Start server
