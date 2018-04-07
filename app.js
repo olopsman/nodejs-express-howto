8aae255592385d72302f2144e0932b52ba84b502
Paulo Orquillo <paulo.orquillo@gmail.com>
2018-04-07 12:57:20 +1200

    Added mongoose reading from db

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// Connect to db
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function() {
+  console.log('Connection to mongo db');
+});
+
+// Check for db errors
+db.on('error', function(err) {
+  console.log(err);
+});
 // Init app
 const app = express();
+
+// Bring in models
+let Article = require('./models/article');
 
 // Load view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -9,14 +27,49 @@ app.set('view engine', 'pug');
 
 // Home route
 app.get('/', function(req, res){
-  res.render('index', {
-    title : 'Hello'
+/*  let articles = [
+    {
+      id:1,
+      title:"Article One",
+      author: "Padma Orquillo",
+      body:"This is article one"
+    },
+    {
+      id:2,
+      title:"Article Two",
+      author: "Amber Orquillo",
+      body:"This is article two"
+    },
+    {
+      id:3,
+      title:"Article three",
+      author: "Pauline Orquillo",
+      body:"This is article three"
+    },
+    {
+      id:3,
+      title:"Article four",
+      author: "Penny Orquillo",
+      body:"This is article four"
+    }
+  ];
+*/
+  Article.find({}, function(err, articles) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title : 'Hello',
+        articles: articles
+      });
+    }
   });
+
 });
 
 // Add route
 app.get('/articles/add', function(req, res) {
-  res.render('add', {
+  res.render('add_articles', {
     title : 'Add article'
   });
 });
